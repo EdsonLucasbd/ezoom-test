82931b914baeb42d974d25b59ef29d8d15cca8d6
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-06-20 18:47:06 -0300

    fix: :bug: whatsapp button z-index

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -5,7 +5,7 @@ export const WhatsappButton = ({ icon }: { icon: Hero['whatsapp'] }) => {
 
   return (
     <Button className="fixed right-0 bottom-0 mr-[29.88px] mb-[49.35px] 
-      size-[44.12px] rounded-full bg-verde-oficial hover:bg-verde-oficial/60 p-2.5"
+      size-[44.12px] rounded-full bg-verde-oficial hover:bg-verde-oficial/60 p-2.5 z-20"
     >
       <img src={icon?.icon?.url} alt="" aria-hidden />
       <p className="sr-only">Falar no whatsapp</p>
