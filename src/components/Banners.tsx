cff8bb55e8b1af23ada87d5925cadc03aba86072
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-06-21 17:45:45 -0300

    style: :lipstick: banners desktop

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -34,16 +34,16 @@ export const Banners = ({ data }: { data: CarouselItemData[] | undefined }) => {
 
       <div className='md:flex w-full hidden'>
         {data && data.map((item) => (
-          <Card key={item.id} className='relative w-full h-full border-none rounded-b-[11.2px]'>
+          <Card key={item.id} className='relative w-[384px] h-[366px] border-none rounded-b-[10px]'>
             <CardContent className='rounded-b-[11.2px] p-0'>
-              <span className={`rounded-b-[5.6px] bg-[${item.color}] h-[8.96px] w-full absolute z-20`} />
+              <span className="rounded-b-[5px] h-[8.96px] w-full absolute z-20" style={{ backgroundColor: item.color ?? '#FF632D' }} />
               <img src={item.image?.url} alt={item.title!} className='w-full h-auto -z-10' />
             </CardContent>
             <CardFooter className='p-0'>
-              <div className="absolute flex flex-col w-full pt-10 px-[43.67px] h-full text-white gap-y-[44.6px] z-40">
-                <p className='font-bold text-[1.26rem] leading-[24.6px] max-w-[288.91px]'>{item.title}</p>
+              <div className="absolute flex flex-col w-full -left-2 pt-14 px-[39px] h-full text-white gap-y-10 z-40">
+                <p className='font-bold text-lg leading-[22px] max-w-[288.91px]'>{item.title}</p>
                 <div className="flex items-center justify-between">
-                  <span className='leading-[24.6px] text-[17.92px] font-normal'>{item.date}</span>
+                  <span className='text-base leading-[22px] font-normal'>{item.date}</span>
                   <img src={arrowRight} aria-hidden alt='' />
                 </div>
               </div>
