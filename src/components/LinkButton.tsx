55f9b2165130211cf63d6bf6e50e8b1a8828ea6f
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-06-21 18:20:35 -0300

    style: :lipstick: dynamic styles

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,10 +1,11 @@
+import { cn } from '../lib/utils'
 import { Button } from './ui/button'
 
-export const LinkButton = ({ children }: { children: string }) => {
+export const LinkButton = ({ children, className }: { children: string, className?: string }) => {
   return (
-    <Button variant="outline" className='rounded-full w-[124px] h-[49px] 
+    <Button variant="outline" className={cn(`rounded-full w-[124px] h-[49px] 
       bg-transparent border-2 hover:bg-white hover:bg-opacity-10 
-      hover:text-white font-bold text-base'>
+      hover:text-white font-bold text-base`, className)}>
       {children}
     </Button>
   )
